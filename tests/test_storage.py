f7688ffc8511e9460f665bca053b8371fc2a7f06
Philip Winnington <ftcpjw@yahoo.com>
2018-11-01 02:22:02 +0000

    added local storage file and test

---
+++ tests/test_storage.py
@@ -0,0 +1,16 @@
+from pgbackup import storage
+import tempfile
+
+def test_storing_file_locally():
+    """
+    Writes contect from one file-like to another
+    """
+    infile = tempfile.TemporaryFile()
+    infile.write(b'Testing')
+    infile.seek(0)
+
+    outfile = tempfile.NamedTemporaryFile(delete=False)
+    storage.local(infile,outfile)
+    with open(outfile.name, 'rb') as f:
+        assert f.read() == b'Testing'
+
